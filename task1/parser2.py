e08adbc0d18de4ea4c0b095acc7e95cc2f06e3dd
demyanov17 <98758895+demyanov17@users.noreply.github.com>
2024-03-21 14:03:16 +0300

    Add files via upload

--- task1/parser2.py
+++ task1/parser2.py
@@ -1,57 +1,123 @@
 import re
+import csv
 import json
 import requests
 from bs4 import BeautifulSoup
 
-HTMLFile = open("source3.html", "r")
 
-# Reading the file 
-index = HTMLFile.read()
+class HTML_2_Parser():
 
+	def __init__(self):
+	    """
+	    Constructor.
 
-soup = BeautifulSoup(index, 'lxml')
+	    :param speed: скорость отряда
+	    """
+	    self.atribute_dict = dict()
 
-name = soup.find("meta", property="og:title")["content"]#.find('content')
-#print(name)
+	def file_parser(self, file_path):
 
-name = str(name)
-# print(name)
-
-#print(index)
-
-# data = re.search(r"window.__INITIAL_STATE__ =(.*);", index).group(1)
-#data = json.loads(data)
-
-# pretty print the data:
-#data = data.split(".parse('")[-1][:-2]
-
-#data.replace()
-#data = json.loads(data)
-#print(data)
-# # pretty print the data:
-#print(json.dumps(data, indent=4))
-#data = data.replace("\\", "")
-#print(data[165030:])
-#["elementInfo"][id, name]
-
-#['elementInfo', 'badges', 'status', 'article', 'allProductsLink', 'prices', 'sectionsTree', 'gallery', 'galleryThumbs', 'linkedProducts', 'hasLeasing', 'creditText',
-#'tradeIn', 'yaPay', 'deliveries', 'banners', 'preOrderInfo', 'trailer', 'security', 'advantages', 'withoutPrice', 'yaPayBadge', 'yaPayBadgeCashback', 'tabs', 'productType',
-#'discountProduct', 'hasAccessoriesOrServices', 'analytics', 'breadcrumbs', 'csrf', 'seo', 'yandexId']
-
-#["elementInfo"][id, name]
-#sectionsTree {'special': {'price': 499990, 'currency': 'RUB'}
-print(soup.find_all("li")[50:])
+		HTMLFile = open(file_path, "r")
+		# Reading the file 
+		index = HTMLFile.read()
+		soup = BeautifulSoup(index, 'lxml')
+		name = soup.find("meta", property="og:description")["content"]#.find('content')
+		name = str(name).split(' в')[0].split("ноутбук ")[-1]
+		print(name)
+		if self.atribute_dict.get("Модель") is None:
+			self.atribute_dict["Модель"] = [name]
+		else:
+			self.atribute_dict["Модель"].append(name)
+		self.extract_data(soup)
+		#print(self.atribute_dict)
 
 
+	def write_csv(self):
+		import pandas as pd
+
+		# Создание DataFrame
+		df = pd.DataFrame(self.atribute_dict)
+
+		# Запись DataFrame в CSV с сохранением индекса
+		df.to_csv('file.csv', index=True)
 
 
-#print(json.loads(data)["tabs"]["list"])
-#print((json.loads(data)["tabs"]["content"][0])["introSpecs"])
+	def extract_data(self, soup):
 
-#important! print((json.loads(data)["tabs"]["content"][0])["introSpecs"])
+		key = None
+		for atibute in soup.find_all("tr"):
+		#Тип экрана, <td>Диагональ экрана</td>
+
+			if "Диагональ экрана" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+				print(atibute)
+
+			if "Процессор" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Серия" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Цвет" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Тип экрана" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Диагональ экрана" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Разрешение экрана" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Яркость" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Количество ядер процессора" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Видеопроцессор" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Количество ядер процессора" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Тип накопителя" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Bluetooth" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Интерфейсы" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Емкость аккумулятора" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Время работы" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Микрофон" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Сканер отпечатка пальца" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Год" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if "Вес" in atibute.text:
+				key, value = atibute.text.split('<td>')[0].split("\n")[1:-1]
+
+			if key is not None:
+				if self.atribute_dict.get(key) is None:
+					self.atribute_dict[key] = [value]
+				else:
+					self.atribute_dict[key].append(value)
+			key = None
 
 
-
-#print(type(json.loads(data)))
-#print(soup.select_one("script", type="application/javascript").contents)
-#popmechanic-desktop
+parser = HTML_2_Parser()
+parser.file_parser("source2/source3.html")
+parser.file_parser("source2/source4.html")
+parser.write_csv()
