7dc8951b320159ca14732b4391d1fd7ad646e5ea
demyanov17 <98758895+demyanov17@users.noreply.github.com>
2024-03-21 17:04:16 +0300

    Add files via upload

--- task1/schema_mapper.py
+++ task1/schema_mapper.py
@@ -2,6 +2,48 @@ import re
 from mrjob.job import MRJob
 
 
+class MR_Gender_Analyzer1(MRJob):
+
+    columns = None
+
+    def mapper(self, _, line):
+
+        """ csv -> [key, value]
+        Args:
+            line(str):
+        Returns:
+            key(str):
+            value(List):
+        """
+
+        from operator import itemgetter
+
+        ids = [2,5,6]
+        column_values = line.split(",")
+        if column_values[1] == "Модель":
+            MR_Gender_Analyzer1.columns = itemgetter(*([1] + ids))(column_values)
+        else:
+            yield (column_values[1], itemgetter(*ids)(column_values)) 
+ 
+
+    def reducer(self, combi, line):
+        line = list(line)
+
+        yield (combi, line)
+
+if __name__ == '__main__':
+    data = []
+    Gender_Analyzer = MR_Gender_Analyzer1(["source1.csv"])
+    with Gender_Analyzer.make_runner() as runner:
+        runner.run()
+        for i, (model, atributes) in enumerate(Gender_Analyzer.parse_output(runner.cat_output())):
+            new_dict = dict(zip(Gender_Analyzer.columns, [model] + next(iter(atributes))))
+            data.append(new_dict)
+    import json
+    json_data = json.dumps(data, ensure_ascii=False, indent=4)
+    with open('source1.json', 'w', encoding='utf8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
+
 class MR_Gender_Analyzer2(MRJob):
 
     columns = None
@@ -27,8 +69,6 @@ class MR_Gender_Analyzer2(MRJob):
  
 
     def reducer(self, combi, line):
-        line = next(iter(line))
-        float(line[1])
         line = list(line)
 
         yield (combi, line)
@@ -44,5 +84,4 @@ if __name__ == '__main__':
     import json
     json_data = json.dumps(data, ensure_ascii=False, indent=4)
     with open('source2.json', 'w', encoding='utf8') as json_file:
-        json.dump(data, json_file, ensure_ascii=False)
-    print(json_data)
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
